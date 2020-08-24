0b214c02853912a5ba0d0a00a1c16ec3abdf7d44
BARATH-VIKRAMAN <41777709+BARATH-VIKRAMAN@users.noreply.github.com>
2020-08-24 18:46:18 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function (req, res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/",function (req,res){
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+
+  var result = Math.round(weight/(height*height)) ;
+
+  res.send("Your BMI value is "+result);
+});
+
+
+app.listen(3000,function (req,res){
+  console.log("Starting port at 3000");
+});
